c590cb2db6d75399de6504c3172b40a2ad6a074d
Skip Khamvongsa <skipk95@gmail.com>
2020-04-03 18:22:54 -0600

    reset key, to not show publicly

--- keys.js
+++ keys.js
@@ -1,6 +1,6 @@
 console.log('this is loaded');
 
 exports.spotify = {
-  id: ceff073607ff429fbeada5149bd25c6e,
-  secret: c1969eb22d2e493eb7c948af7e8f93ef
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
 };
